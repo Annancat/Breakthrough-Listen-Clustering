9e55144bf4ae48cbbe4d557c2ddf183747dbed76
uniq2_2022 <109153051+runningAccount@users.noreply.github.com>
2022-07-28 11:46:03 +0100

    Test Run of createImages

--- createImages.py
+++ createImages.py
@@ -1,6 +1,6 @@
-#import matplotlib
-#matplotlib.use('TkAgg')
-#import matplotlib.pyplot as plt
+# import matplotlib
+# matplotlib.use('TkAgg')
+# import matplotlib.pyplot as plt
 import os, glob, shutil
 import blimpy.calcload
 import modified_functions as modif
@@ -16,6 +16,19 @@ Separated from api_requests.py so that the urls do not need to be gathered throu
 Assumes that it has access to the files via converting the urls and does not need to download them - downloading them
 as the converter run would be even more costly than it already is!
 """
+
+
+def makeFinalImage(name="", splice=-1, freq=-1):
+    for i in range(0, splice + 1):
+        try:
+            print("Creating final image for splice " + str(i) +
+                  " of current target")
+            modif.combine_pngs(splice=i)
+            print("Completed creating images for cadence " + cur_url)
+        except:
+            print("Skipping creating final image for cadence " + cur_url + "...")
+
+
 # TODO find way to download files without ssh as a backup if someone does not have access.
 if __name__ == '__main__':
     api = api_requests.OpendataAPI()
@@ -23,62 +36,66 @@ if __name__ == '__main__':
     os.mkdir("tempImages")
     shutil.rmtree("images")
     os.mkdir("images")
-    urls = open("urls_cleaned.txt", "r")  # From api_requests.py
+    urls = open("urls_cleaned.txt", "r")  # URLs from api_requests.py
     cur_url = urls.readline()
-    test = subprocess.check_output(["echo", "Hello World"])
-    print(str(test)[2:-3] + " test")
+    freqRange = 200  # The frequency range in Mhz for each image
 
-    freqRange = 300  # The frequency range in Mhz for each image
     while cur_url != "":
         cadenceFiles = api.get_cadence(cur_url)
         observation = 0
         name = ""
-        splice = 0 #initialised here to avoid errors.
+        splice = 0  # initialised here to avoid errors.
+
         for i in range(0, len(cadenceFiles)):  # Full observation split into 3 files for different frequency.
+
             if "0001.fil" in cadenceFiles[i]["url"]:
-                path = cadenceFiles[i]["url"].replace("http://",
-                                                      "/mnt_").replace(".ssl.berkeley.edu",
-                                                                       "/datax").replace("datax/dl2", "datax2/dl")
-                print(path)
-                print(os.path.exists(path))
+                # Converts url into location on server.
+                path = cadenceFiles[i]["url"].replace(
+                    "http://", "/mnt_").replace(".ssl.berkeley.edu", "/datax").replace("datax/dl2", "datax2/dl")
+
                 if not os.path.exists(path):
-                    print("Path does not exist!\n\n")
-                """try:
-                    # Converts url into location on server.
-                    script = subprocess.check_output(["~scroft/parsefile", str(cadenceFiles["data"][i]["url"])])
-                    # TODO: ask for script to replicate work for future use.
-                    path = str(script)[2:-3]  # Strips irrelevant characters from the returned output.
-                    print(path)
+                    print("Path" + path + "does not exist!\n\n")
+                    continue
+
+                try:
                     # Max load is in gb. Only uses the resources it needs to load the .fil
                     fil = Waterfall(path, max_load=blimpy.calcload.calc_max_load(path))
                     name = fil.header["source_name"]
                     splice = 0
                     freqs = fil.get_freqs()
                     maxFreq = freqs[0]
-                    minFreq = freqs[-1]
-                    curFreq = minFreq
+                    curFreq = freqs[-1]
+
                     while curFreq <= maxFreq - freqRange:
-                        modif.waterfall_png(fil, "tempImages/" + name + str(observation) + "_" + str(splice),
+                        modif.waterfall_png(fil, "tempImages/" +
+                                            name + "_FREQ_" +
+                                            fil.header["center_freq"] + "_" +
+                                            str(observation) + "_" +
+                                            str(splice),
                                             f_start=curFreq, f_stop=curFreq + freqRange)
                         curFreq += freqRange
                         splice += 1
-                    modif.waterfall_png(fil, "tempImages/" + name + str(observation) + "_" + str(splice),
+
+                    modif.waterfall_png(fil, "tempImages/" +
+                                        name + "_FREQ_" +
+                                        fil.header["center_freq"] + "_" +
+                                        str(observation) + "_" +
+                                        str(splice),
                                         f_start=maxFreq - (maxFreq - curFreq), f_stop=maxFreq)
+
+                    if i != 0 and fil.header["center_freq"] != center_freq:
+                        makeFinalImage(splice=splice, freq=center_freq)
+
+                    center_freq = fil.header["center_freq"]
                     observation += 1
                     print("\n-----------------------------")
                     print("Completed temporary images.")
                     print("-----------------------------\n")
+
                 except:
                     print("Skipping cadence file at url " +
                           cur_url + "as the file size is too large to compute or file cannot be found.")
 
+        makeFinalImage(splice=splice, freq=center_freq)
 
-        for i in range(0, splice + 1):
-            try:
-                print("Creating final image for splice " + str(i) +
-                        " of target " + name)
-                modif.combine_pngs(name, i)
-                print("Completed creating images for cadence " + cur_url)
-            except:
-                print("Skipping creating final image for cadence " + cur_url + "...")"""
         cur_url = urls.readline()
--- modified_functions.py
+++ modified_functions.py
@@ -83,7 +83,7 @@ def waterfall_png(wf, name, f_start=None, f_stop=None, **kwargs):
     # Save waterfall plot at location
     plt.imsave(name, normalized_plot_data,**kwargs)
 
-def combine_pngs(name,splice = -1):
+def combine_pngs(name = "",splice = -1):
     r"""
     Create one png from the On and Off observation pngs created from waterfall_png.
     Parameters
@@ -96,10 +96,19 @@ def combine_pngs(name,splice = -1):
     Images get combined vertically.
     """
     # With current layout, there should always only be 6 temp images max per call.
-    if splice != -1:
-        files = sorted(glob.glob(os.path.join("tempImages", name + "*_" + str(splice) + ".png")))
+    # However it will not crash if given more.
+    if name == "":
+        if splice != -1:
+            files = sorted(glob.glob(os.path.join("tempImages", str(splice) + ".png")))
+            name = files[0][:-7]
+        else:
+            files = sorted(glob.glob(os.path.join("tempImages", "*.png")))
+            name = files[0][:-5]
     else:
-        files = sorted(glob.glob(os.path.join("tempImages", name + "*.png")))
+        if splice != -1:
+            files = sorted(glob.glob(os.path.join("tempImages", name + "*_" + str(splice) + ".png")))
+        else:
+            files = sorted(glob.glob(os.path.join("tempImages", name + "*.png")))
 
     images = [Image.open(x) for x in files]
     widths, heights = zip(*(i.size for i in images))
