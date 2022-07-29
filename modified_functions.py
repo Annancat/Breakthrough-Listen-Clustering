d042a4e5ac4deeaaf7f6e67fb36bedef45d6e741
uniq2_2022 <109153051+runningAccount@users.noreply.github.com>
2022-07-29 10:22:16 +0100

    Test Run of createImages

--- modified_functions.py
+++ modified_functions.py
@@ -73,7 +73,7 @@ def waterfall_png(wf, name, f_start=None, f_stop=None, **kwargs):
 
     # plot and scale intensity (log vs. linear)
     if "_OFF" in name:
-        kwargs["viridis"] = kwargs.get("cmap", "viridis")
+        kwargs["cmap"] = kwargs.get("cmap", "inferno")
     else:
         kwargs["cmap"] = kwargs.get("cmap", "viridis")
     plot_data = 10.0 * np.log10(plot_data)
