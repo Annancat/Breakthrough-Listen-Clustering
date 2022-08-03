f6c26194864edbd2908cfe9e53929dbf816d93d2
uniq2_2022 <109153051+runningAccount@users.noreply.github.com>
2022-08-03 11:49:56 +0100

    Attempting to normalise histograms

--- modified_functions.py
+++ modified_functions.py
@@ -141,12 +141,13 @@ def combine_pngs(name="", part=-1, freq=-1):
     length = 0
     for i in range(0, len(images_on)):
         if y_offset ==0:
+            ref = images_on[i]
             new_im.paste(Image.fromarray(images_on[i]), (0, y_offset))
             y_offset += heights[i]
         else:
-            new_im.paste(Image.fromarray(match_histograms(images_on[i],images_off[i-1],channel_axis = -1),'RGB'), (0, y_offset))
+            new_im.paste(Image.fromarray(match_histograms(images_on[i],ref,channel_axis = 3),'RGB'), (0, y_offset))
             y_offset += heights[i]
-        new_im.paste(Image.fromarray(match_histograms(images_off[i],images_on[i],channel_axis = -1),'RGB'), (0, y_offset))
+        new_im.paste(Image.fromarray(match_histograms(images_off[i],ref,channel_axis = 3),'RGB'), (0, y_offset))
         y_offset += heights_off[i]
         length +=2
         if length >= 6:
