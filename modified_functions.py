90516d536a7f65343cbee7ca492fc2fad77b569b
uniq2_2022 <109153051+runningAccount@users.noreply.github.com>
2022-08-04 10:32:18 +0100

    Refactoring/cleaning up code + added simple kmeans cnn

--- modified_functions.py
+++ modified_functions.py
@@ -132,9 +132,9 @@ def combine_pngs(name="", part=-1, freq=-1):
             y_offset += heights_off[i]
 
         else:
-            new_im.paste(Image.fromarray(match_histograms(asarray(images_on[i]), ref_on)))
+            new_im.paste(Image.fromarray(match_histograms(asarray(images_on[i]), ref_on),'RGB'),(0, y_offset))
             y_offset += heights_on[i]
-            new_im.paste(Image.fromarray(match_histograms(asarray(images_off[i]), ref_off)))
+            new_im.paste(Image.fromarray(match_histograms(asarray(images_off[i]), ref_off),'RGB'),(0, y_offset))
             y_offset += heights_off[i]
 
         length += 2
